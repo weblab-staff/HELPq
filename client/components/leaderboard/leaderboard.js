50adf31e99e41e8ae6cee7be4d37f77cab81ce37
andrew <andrewliu278@gmail.com>
2024-01-30 22:52:35 -0500

    change sig figs

--- client/components/leaderboard/leaderboard.js
+++ client/components/leaderboard/leaderboard.js
@@ -1,4 +1,4 @@
-Template.leaderboard.onCreated(function(){
+Template.leaderboard.onCreated(function () {
   this.subscribe("ticketData");
   this.subscribe("allMentors");
   this.rows = new ReactiveVar();
@@ -8,26 +8,30 @@ Template.leaderboard.onCreated(function(){
 });
 
 Template.leaderboard.helpers({
-  topMentors: function(){
+  topMentors: function () {
     // Return the top number of mentors
     return topMentors(Template.instance().rows.get());
-  }
+  },
 });
 
 // Ranking algorithm based on number of ratings and quality of ratings.
-function topMentors(num){
+function topMentors(num) {
   var mentors = {};
   var tickets = Tickets.find({
-    status: "COMPLETE"
-  }).fetch().filter(function(t){return t.rating > 0});
+    status: "COMPLETE",
+  })
+    .fetch()
+    .filter(function (t) {
+      return t.rating > 0;
+    });
 
   // Each mentor has a set of ratings
-  tickets.forEach(function(t){
-    if (t.claimId){
+  tickets.forEach(function (t) {
+    if (t.claimId) {
       if (!mentors[t.claimId]) {
         mentors[t.claimId] = {
-          ratings: []
-        }
+          ratings: [],
+        };
       }
       mentors[t.claimId].ratings.push(t.rating);
     }
@@ -35,26 +39,25 @@ function topMentors(num){
 
   var ids = Object.keys(mentors);
   return ids
-      .filter(function(id){
-        return Meteor.users.findOne({_id: id, 'profile.mentor': true});
-      })
-      .map(function(id){
-        return {
-          profile: Meteor.users.findOne({_id: id}).profile,
-          rating: laplaceSmooth(mentors[id].ratings),
-          numTickets: mentors[id].ratings.length
-        }
-      })
-      .sort(function(a, b){
-        return b.rating - a.rating;
-      })
-      .slice(0, num);
-
+    .filter(function (id) {
+      return Meteor.users.findOne({ _id: id, "profile.mentor": true });
+    })
+    .map(function (id) {
+      return {
+        profile: Meteor.users.findOne({ _id: id }).profile,
+        rating: laplaceSmooth(mentors[id].ratings),
+        numTickets: mentors[id].ratings.length,
+      };
+    })
+    .sort(function (a, b) {
+      return b.rating - a.rating;
+    })
+    .slice(0, num);
 }
 
-function laplaceSmooth(x){
+function laplaceSmooth(x) {
   var alpha = 6,
-      beta  = 2,
-      sum  = stats.sum(x);
-  return ((sum + alpha)/(x.length + beta)).toFixed(1);
+    beta = 2,
+    sum = stats.sum(x);
+  return ((sum + alpha) / (x.length + beta)).toFixed(3);
 }
